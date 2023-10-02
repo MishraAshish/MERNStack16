3d5d02e6e5e604d41f878834c4115a08acbf22b3
Ashish <ash.ishu025@gmail.com>
2023-10-02 21:49:49 +0530

    creating cart component

---
+++ reactapp/src/application/CommonComponent/ProjectEnhancement.js
@@ -0,0 +1,16 @@
+// 02-10-2023 - Practice
+
+// Cart Implementation
+// Create New Cart Component using react hooks, functional component
+// Each Item in this component should be added from Product Component 
+// Now each Product should have general fields, like Name, Description, Rating, Price, Category (New Product Document/ Collection)
+// In Product component each item when we click to display details should also have a button "Add To Item" on click should add to New Cart
+// On Cart Component, Button for save to Checkout should save the cart item to database (New Cart Document/ Collection)
+
+
+// 29-09-2023 - Create Product and Display Them
+
+// Create a product component using functional component and hooks (try using use state to read default values from reducer)
+// Create a form to allow user to submit Product Details - name, price, desc, rating
+// Create an action method to add the detail to database using a server api
+// Server Side - Create product router and api to save the product using productdatamodel
