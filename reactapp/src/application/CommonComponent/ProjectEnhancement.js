13b2224f4f5939af90bbaf3baf0e656d2766db04
Ashish <ash.ishu025@gmail.com>
2023-10-12 21:57:26 +0530

    cancelled orders

--- reactapp/src/application/CommonComponent/ProjectEnhancement.js
+++ reactapp/src/application/CommonComponent/ProjectEnhancement.js
@@ -1,3 +1,9 @@
+// 12-10-2023 - Cancelled Orders
+// Save the order to CancelledOrders collection (should have userid, cancelled, dateTime)
+// Make API to Save and Fetch from CancelledOrders
+// Make a component CancelledOrders to Show all cancelled Orders of current user in Latest First
+// Add a button to Buy Again, (also show a message - This offer is much more exciting)
+// Upon Adding this should get appended to the existing Cart that is shown in Carts App
 
 
 // 11-10-2023 - Recentorder Page 
